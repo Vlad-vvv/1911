bf8641ed167f7d1efc4420a5ceb2557d5447598c
Vlad-vvv <vrnnvlad@gmail.com>
2023-01-31 20:01:26 +0200

    Home work3

--- Lesson3/Lesson3.py
+++ Lesson3/Lesson3.py
@@ -5,7 +5,8 @@ class Student:
     def __init__(self, name):
         self.name = name
         self.progress = 1.0
-        self.fun = 50.0
+        self.fun = 70.0
+        self.money = 200
         self.alive = True
 
     def to_study(self):
@@ -16,11 +17,19 @@ class Student:
     def to_sleep(self):
         print("Я пішов спати")
         self.fun += 3
+        self.money -=4
 
     def to_have_fun(self):
         print("Я розважаюсь ")
         self.progress -= 0.1
         self.fun += 5
+        self.money -= 25
+
+    def to_have_money(self):
+        print("Я заробряю гроші")
+        self.money += 20
+        self.progress += 0.1
+        self.fun -= 5
 
     def is_alive(self):
         if self.progress < 0:
@@ -32,21 +41,29 @@ class Student:
         if self.fun < 40:
             print("В мене депрессія")
             self.alive = False
+        if self.money < 20:
+                print("У мене мало грошей")
+                self.money += 20
+                self.progress += 0.1
+                self.fun -= 5
 
     def end_of_day(self):
         print(f'Задоволенність - {self.fun}')
         print(f'Прогрес        - {round(self.progress)}')
+        print(f'Гроші          - {round(self.money)}')
 
     def live(self, day):
         info = f'День {day} з життя {self.name}'
         print(f'{info:=^40}')
-        choice = random.randint(1, 3)
+        choice = random.randint(1, 4)
         if choice == 1:
             self.to_study()
         elif choice == 2:
             self.to_sleep()
         elif choice == 3:
             self.to_have_fun()
+        elif choice == 4:
+            self.to_have_money()
 
         self.end_of_day()
         self.is_alive()
