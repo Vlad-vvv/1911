8aac8bf3c712cbe7f8d08dba8fce1c0441f18dad
Vlad-vvv <vrnnvlad@gmail.com>
2023-02-25 13:15:09 +0200

    home_work10

--- 10/10.py
+++ 10/10.py
@@ -4,11 +4,12 @@ connection = sqlite3.connect("database.sl3", 5)
 cur = connection.cursor()
 
 #cur.execute("create table users (id int primary key not null, login text, password text);")
-#cur.execute("insert into users (id, login, password) values (2, 'user2', 'paqw');")
-cur.execute("select * from users")
+#cur.execute("insert into users (id, login, password) values (6, 'Admin', '12345678901234567890');")
+cur.execute("select password from users where login='Admin';")
 connection.commit()
 res = cur.fetchall()
 print(res)
+
 #print(connection)
 #print(cur)
 
